f25fc9558d7bb6b50795011884bb038efc27b450
Kyle Pace <kpace2296@gmail.com>
2020-03-11 14:51:13 -0500

    finished most of mvp"

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const shortid = require('shortid')
 
 const server = express();
 
-    const users = [
+    let users = [
         {
             id: 1,
             name: 'Bob',
@@ -15,6 +16,8 @@ const server = express();
         }
     ];
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
     res.send('Test')
 });
@@ -23,19 +26,53 @@ server.listen(5000, () => {
     console.log('server running on 5000')
 })
 
+// R - Read (CRUD)
 server.get('/users', (req, res) => {
 
     res.status(200).json(users)
 })
 
-server.post('/users', (req, res) => {
-    const users = req.body;
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
 
-    users(users)
-        .then(user => {
-            res.status(201).json({success:true, user})
-        })
-        .catch(err => {
-            res.status(500).json({success:false, err})
-        })
+    res.status(200).json(users[id])
 })
+
+// C - Create (CRUD)
+server.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    newUser.id = shortid.generate();
+
+    users.push(newUser)
+
+    res.status(201).json(newUser)
+})
+
+// D - Delete (CRUD)
+server.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = users.find(user => user.id === id);
+
+    if (deleted) {
+        users = users.filter(user => user.id !== id);
+    } else {
+        res.status(404).json({ success:false, message:'user id was not found '});
+    }
+});
+
+// U - Update (CRUD)
+server.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const changedUser = req.body;
+
+    let index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        users[index] = changedUser;
+        res.status(200).json(users[index]);
+    } else {
+        res.status(404).json({ success:false, message:'user id not found' });
+    }
+});
