e90fbe7ea653bd079a1c017c0e20fcdc26fc3c49
Kyle Pace <kpace2296@gmail.com>
2020-03-11 22:07:34 -0500

    finished mvp and some of stretch

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const shortid = require('shortid')
 
-const server = express();
+const server = express()
+const cors = require('cors')
 
-    const users = [
+    let users = [
         {
             id: 1,
             name: 'Bob',
@@ -15,6 +17,9 @@ const server = express();
         }
     ];
 
+server.use(express.json());
+server.use(cors());
+
 server.get('/', (req, res) => {
     res.send('Test')
 });
@@ -23,19 +28,66 @@ server.listen(5000, () => {
     console.log('server running on 5000')
 })
 
+// R - Read (CRUD)
 server.get('/users', (req, res) => {
-
-    res.status(200).json(users)
+    if (users) {
+       res.status(200).json(users) 
+    } else {
+        res.status(404).json({errorMessage: "The users information could not be retrieved."})
+    }
 })
 
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (users[id]) {
+    res.status(200).json(users[id])
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist." })
+    }
+})
+
+// C - Create (CRUD)
 server.post('/users', (req, res) => {
-    const users = req.body;
+    const { name, bio } = req.body;
 
-    users.push(users)
-        .then(user => {
-            res.status(201).json({success:true, user})
-        })
-        .catch(err => {
-            res.status(500).json({success:false, err})
-        })
+    newUser.id = shortid.generate();
+
+    users.push(req.body)
+
+    if (name, bio) {
+        res.status(201).json(req.body)
+    } else if (!name || !bio){
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    } else {
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database"})
+    }
 })
+
+// D - Delete (CRUD)
+server.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = users.find(user => user.id === id);
+
+    if (deleted) {
+        users = users.filter(user => user.id !== id);
+    } else {
+        res.status(404).json({ success:false, message:'user id was not found '});
+    }
+});
+
+// U - Update (CRUD)
+server.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const changedUser = req.body;
+
+    let index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        users[index] = changedUser;
+        res.status(200).json(users[index]);
+    } else {
+        res.status(404).json({ success:false, message:'user id not found' });
+    }
+});
